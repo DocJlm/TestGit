2440203750cb11953cdd170b1e786d387075904b
DOC.JLM <15516365393@163.com>
2025-02-21 16:16:04 +0800

    第一次提交

---
+++ lqCup/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author ZQC
+ * @date 2025-02-21
+ * @project Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ lqCup/src/com/coder/zqc/Student.java
@@ -0,0 +1,9 @@
+package com.coder.zqc;
+
+/**
+ * @author ZQC
+ * @date 2025-02-21
+ * @project TestGit
+ */
+public class Student {
+}
